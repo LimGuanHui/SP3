94dc7e844625d9b0dc5bfac4815a92e895eecc7b
unknown <508@CL38G405.sit.nyp.edu.sg>
2016-08-22 17:55:44 +0800

    Some small changes

--- Character/Movement.cpp
+++ Character/Movement.cpp
@@ -5,7 +5,6 @@ namespace MOVEMENT
 	CMovement::CMovement()
 		: jumpspeed(0)
 		, AnimationCounter(0)
-		, OnGround(true)
 		, InAir(false)
 		, velocity(5,0,0)
 	{
@@ -147,7 +146,7 @@ namespace MOVEMENT
 		}
 		else
 		{
-			position.x = position.x + (velocity.x * timeDiff);
+			position.x += (velocity.x * timeDiff);
 			AnimationInvert = false;
 			AnimationCounter++;
 			if (AnimationCounter > 2)
